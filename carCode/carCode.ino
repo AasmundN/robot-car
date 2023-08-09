4979ba63f3e7735476d0fb700d117b68a67de24a
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-09 12:14:19 +0200

    feat: change logos
    - Replace CoT logos with Zumo car sketches.
    - Fix code formatting.

--- carCode/carCode.ino
+++ carCode/carCode.ino
@@ -1,76 +1,65 @@
 #include "Car.h"
 
-//nettverksnavn og nettverkspassord byttes ut med navn og passord for det aktuelle nettverket
+// nettverksnavn og nettverkspassord byttes ut med navn og passord for det aktuelle nettverket
 Car car("ruter_cot", "ESP_32_is_best");
 
-void setup() { 
-  //BLACK for svart linje og WHITE for hvit linje
-  car.initCar(BLACK);
+void setup() {
+   // BLACK for svart linje og WHITE for hvit linje
+   car.initCar(BLACK);
 }
 
-//lag variabler her
+// lag variabler her
 bool linemode = false;
 
+void loop() { // ikke fjern denne linjen!
 
-void loop() { //ikke fjern denne linjen!
+   // skriv kode for å sende data her
 
-  //skriv kode for å sende data her
-
-  
-  if(linemode == true) {
-    //skriv linjefølger-kode her
-    
-  }
+   if (linemode == true) {
+      // skriv linjefølger-kode her
+   }
 }
 
-//dette er koden for kjøre framover
-void w(bool button) { //ikke fjern denne linjen!
-  if(button==DOWN) {
-    //dette skjer når knappen trykkes ned
-    drive(50,50); 
-  }
-  if(button==UP) {
-    //dette skjer når knappen slippes opp
-    drive(0,0);
-  }
+// dette er koden for kjøre framover
+void w(bool button) { // ikke fjern denne linjen!
+   if (button == DOWN) {
+      // dette skjer når knappen trykkes ned
+      drive(50, 50);
+   }
+   if (button == UP) {
+      // dette skjer når knappen slippes opp
+      drive(0, 0);
+   }
 }
 
-void a(bool button) { //ikke fjern denne linjen!
-  //BEGYNN HER!
-
+void a(bool button) { // ikke fjern denne linjen!
+                      // BEGYNN HER!
 }
 
-void s(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void s(bool button) { // ikke fjern denne linjen!
+                      // skriv kode her
 }
 
-void d(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void d(bool button) { // ikke fjern denne linjen!
+                      // skriv kode her
 }
 
-void e(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void e(bool button) { // ikke fjern denne linjen!
+                      // skriv kode her
 }
 
-void q(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void q(bool button) { // ikke fjern denne linjen!
+                      // skriv kode her
 }
 
-void triangle(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void triangle(bool button) { // ikke fjern denne linjen!
+                             // skriv kode her
 }
 
-void circle(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void circle(bool button) { // ikke fjern denne linjen!
+                           // skriv kode her
 }
 
-void square(bool button) { //ikke fjern denne linjen!
-  //skriv kode her
-
+void square(bool button) { // ikke fjern denne linjen!
+                           // skriv kode her
 }
--- zumoCode/zumoCode.ino
+++ zumoCode/zumoCode.ino
@@ -1,13 +1,13 @@
 #include <Zumo32U4.h>
 
-//https://www.pololu.com/docs/0J63
+// https://www.pololu.com/docs/0J63
 
 #define LED_PIN 14
 
-const int ENA = 10; //Motor 1
-const int IN1 = 16; //Wheel direction 1
-const int ENB = 9; //Motor 2
-const int IN2 = 15; //Wheel direction 2
+const int ENA = 10; // Motor 1
+const int IN1 = 16; // Wheel direction 1
+const int ENB = 9;  // Motor 2
+const int IN2 = 15; // Wheel direction 2
 
 int actual_l_val = 0;
 int actual_r_val = 0;
@@ -19,7 +19,7 @@ int acc_const = 50;
 int accPerSec = 100;
 unsigned long prevAccMillis = 0;
 
-int maxSpeed = 200; 
+int maxSpeed = 200;
 bool isCalibrated = false;
 bool lineColor = false;
 
@@ -27,12 +27,12 @@ int encoderValLeft, encoderValRight;
 float stallThresholdLeft = 100;
 float stallThresholdRight = 100;
 
-bool prevEncoderVals[3] = {0,0,0};
+bool prevEncoderVals[3] = {0, 0, 0};
 bool emergancyStop = false;
 unsigned long lastStop = 0;
 int stopDuration = 2;
 
-const int lineSensorValues[5] = {0,0,0,0,0};
+const int lineSensorValues[5] = {0, 0, 0, 0, 0};
 
 const unsigned int microSecPerByte = ceil(10.0 / 115200.0 * 100000);
 
@@ -40,7 +40,7 @@ unsigned long lastSpeedChange = 0;
 int speedChangePerSec = 2;
 
 unsigned long prevReadMillis = 0;
-int readPerSec = 20; //må være mindre enn 71, da hver read tar 13-14ms
+int readPerSec = 20; // må være mindre enn 71, da hver read tar 13-14ms
 
 unsigned long prevStallMillis = 0;
 int stallPerSec = 4;
@@ -55,207 +55,205 @@ int8_t data[2];
 int8_t pos;
 
 void calibrateLineSensors() {
-  for(uint8_t i = 0; i < 120; i++) {
-    if (i > 30 && i <= 90) {
-      drive(-100,100);
-    } else {
-      drive(100,-100);
-    }
-    lineSensors.calibrate();
-  }
+   for (uint8_t i = 0; i < 120; i++) {
+      if (i > 30 && i <= 90) {
+         drive(-100, 100);
+      } else {
+         drive(100, -100);
+      }
+      lineSensors.calibrate();
+   }
 
-  drive(0,0);
+   drive(0, 0);
 }
 
 void drive(int8_t l_val, int8_t r_val) {
-  digitalWrite(IN1, l_val>0?0:1);
-  digitalWrite(IN2, r_val>0?0:1);
+   digitalWrite(IN1, l_val > 0 ? 0 : 1);
+   digitalWrite(IN2, r_val > 0 ? 0 : 1);
 
-  l_val = (maxSpeed/100)*abs(l_val);
-  r_val = (maxSpeed/100)*abs(r_val);
+   l_val = (maxSpeed / 100) * abs(l_val);
+   r_val = (maxSpeed / 100) * abs(r_val);
 
-  analogWrite(ENA, l_val);
-  analogWrite(ENB, r_val);
+   analogWrite(ENA, l_val);
+   analogWrite(ENB, r_val);
 }
 
-void accelerate(int8_t l_val, int8_t r_val){
-  if(abs(l_val-ideal_l_val)>25||abs(r_val-ideal_r_val)>25) {
-    for(auto val : prevEncoderVals) val = false;
-    lastSpeedChange = millis();
-  }
-  ideal_r_val = r_val;
-  ideal_l_val = l_val;  
+void accelerate(int8_t l_val, int8_t r_val) {
+   if (abs(l_val - ideal_l_val) > 25 || abs(r_val - ideal_r_val) > 25) {
+      for (auto val : prevEncoderVals)
+         val = false;
+      lastSpeedChange = millis();
+   }
+   ideal_r_val = r_val;
+   ideal_l_val = l_val;
 }
 
 void setup() {
-  drive(0,0);
+   drive(0, 0);
 
-  lineSensors.initFiveSensors();
-  proxSensors.initFrontSensor();
+   lineSensors.initFiveSensors();
+   proxSensors.initFrontSensor();
 
-  Serial1.begin(115200);
-  Serial.begin(115200);
+   Serial1.begin(115200);
+   Serial.begin(115200);
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+   pinMode(LED_PIN, OUTPUT);
+   digitalWrite(LED_PIN, LOW);
 
-  delay(100);
-  buzzer.play(">g32>>>>>c32");
+   delay(100);
+   buzzer.play(">g32>>>>>c32");
 }
 
 void loop() {
 
-  if(prevEncoderVals[0]&&prevEncoderVals[1]&&prevEncoderVals[2]&&!emergancyStop) {
-    Serial.println("Emergancystop on!");
-    emergancyStop = true;
-    lastStop = millis();
-    lastSpeedChange = millis();
-    drive(0,0);
-  }
+   if (prevEncoderVals[0] && prevEncoderVals[1] && prevEncoderVals[2] && !emergancyStop) {
+      Serial.println("Emergancystop on!");
+      emergancyStop = true;
+      lastStop = millis();
+      lastSpeedChange = millis();
+      drive(0, 0);
+   }
 
-  if(emergancyStop&&(millis()-lastStop)>(1000*stopDuration)) {
-    Serial.println("Emergancystop off!");
-    emergancyStop = false;
-    for(auto val : prevEncoderVals) val = false;
-  }
+   if (emergancyStop && (millis() - lastStop) > (1000 * stopDuration)) {
+      Serial.println("Emergancystop off!");
+      emergancyStop = false;
+      for (auto val : prevEncoderVals)
+         val = false;
+   }
 
-  if((millis()-prevAccMillis)>(1000/accPerSec)&&!emergancyStop) {
+   if ((millis() - prevAccMillis) > (1000 / accPerSec) && !emergancyStop) {
 
-    if(actual_l_val < ideal_l_val) 
-      actual_l_val += acc_const;
-    
-    if(actual_r_val < ideal_r_val) 
-      actual_r_val += acc_const;
-    
-    if(actual_l_val > ideal_l_val) 
-      actual_l_val -= acc_const;
-    
-    if(actual_r_val > ideal_r_val) 
-      actual_r_val -= acc_const;
-    
+      if (actual_l_val < ideal_l_val)
+         actual_l_val += acc_const;
 
-    if(abs(actual_r_val - ideal_r_val) <= acc_const) 
-      actual_r_val = ideal_r_val;
-    
-    if(abs(actual_l_val - ideal_l_val) <= acc_const) 
-      actual_l_val = ideal_l_val;
-    
+      if (actual_r_val < ideal_r_val)
+         actual_r_val += acc_const;
 
-    drive(actual_l_val, actual_r_val);
+      if (actual_l_val > ideal_l_val)
+         actual_l_val -= acc_const;
 
-    prevAccMillis = millis();
-    
-  } else if(emergancyStop) {
-    drive(0,0);
-  }
+      if (actual_r_val > ideal_r_val)
+         actual_r_val -= acc_const;
 
-  if((millis()-prevStallMillis)>(1000/stallPerSec)&&(millis()-lastSpeedChange)>(1000/speedChangePerSec)&&!emergancyStop) {
+      if (abs(actual_r_val - ideal_r_val) <= acc_const)
+         actual_r_val = ideal_r_val;
 
-    encoderValLeft = sqrt(pow(encoders.getCountsAndResetLeft(),2));
-    encoderValRight = sqrt(pow(encoders.getCountsAndResetRight(),2));
+      if (abs(actual_l_val - ideal_l_val) <= acc_const)
+         actual_l_val = ideal_l_val;
 
-    stallThresholdLeft = ((2*abs(actual_l_val)-150)+abs(2*abs(actual_l_val)-150))/2+100;
-    stallThresholdRight = ((2*abs(actual_r_val)-150)+abs(2*abs(actual_r_val)-150))/2+100;
+      drive(actual_l_val, actual_r_val);
 
-    prevEncoderVals[2] = prevEncoderVals[1];
-    prevEncoderVals[1] = prevEncoderVals[0];
-    
-    if((encoderValLeft<stallThresholdLeft&&actual_l_val&&ideal_l_val)
-      ||(encoderValRight<stallThresholdRight&&actual_r_val&&ideal_r_val))
-      prevEncoderVals[0] = true;
-    else
-      prevEncoderVals[0] = false;
+      prevAccMillis = millis();
+   } else if (emergancyStop) {
+      drive(0, 0);
+   }
 
-   //  Serial.print(encoderValLeft);
-   //  Serial.print("  ");
-   //  Serial.print(stallThresholdLeft);
-   //  Serial.print("  ");
-   //  Serial.print(encoderValRight);
-   //  Serial.print("  ");
-   //  Serial.print(stallThresholdRight);
-   //  Serial.print("  ");
-   //  Serial.print(prevEncoderVals[0]);
-   //  Serial.print("  ");
-   //  Serial.print(prevEncoderVals[1]);
-   //  Serial.print("  ");
-   //  Serial.println(prevEncoderVals[2]);  
+   if ((millis() - prevStallMillis) > (1000 / stallPerSec) && (millis() - lastSpeedChange) > (1000 / speedChangePerSec) && !emergancyStop) {
 
-    prevStallMillis = millis();
-  }
+      encoderValLeft = sqrt(pow(encoders.getCountsAndResetLeft(), 2));
+      encoderValRight = sqrt(pow(encoders.getCountsAndResetRight(), 2));
 
-  if(Serial1.available()) {
-    char received = (char)Serial1.read();
-    //Serial.print("recieved command: ");
-    //Serial.println(received); //Print data to Serial Monitor
+      stallThresholdLeft = ((2 * abs(actual_l_val) - 150) + abs(2 * abs(actual_l_val) - 150)) / 2 + 100;
+      stallThresholdRight = ((2 * abs(actual_r_val) - 150) + abs(2 * abs(actual_r_val) - 150)) / 2 + 100;
 
-    switch(received) {
-      case 'k': //Kjør!
-        //Serial.println('k');
-        while(Serial1.available() < 2){
-          delayMicroseconds(microSecPerByte);
-        }
+      prevEncoderVals[2] = prevEncoderVals[1];
+      prevEncoderVals[1] = prevEncoderVals[0];
 
-        for(int i = 0; i < 2; i++){
-          if(Serial1.available()){
-            data[i] = Serial1.read();
-            //Serial.print(data[i]);
-          }
-        }
+      if ((encoderValLeft < stallThresholdLeft && actual_l_val && ideal_l_val) || (encoderValRight < stallThresholdRight && actual_r_val && ideal_r_val))
+         prevEncoderVals[0] = true;
+      else
+         prevEncoderVals[0] = false;
 
-        // Serial.print(data[0]);
-        // Serial.print("  ");
-        // Serial.println(data[1]);
-        accelerate(data[0], data[1]);
-        break;
+      //  Serial.print(encoderValLeft);
+      //  Serial.print("  ");
+      //  Serial.print(stallThresholdLeft);
+      //  Serial.print("  ");
+      //  Serial.print(encoderValRight);
+      //  Serial.print("  ");
+      //  Serial.print(stallThresholdRight);
+      //  Serial.print("  ");
+      //  Serial.print(prevEncoderVals[0]);
+      //  Serial.print("  ");
+      //  Serial.print(prevEncoderVals[1]);
+      //  Serial.print("  ");
+      //  Serial.println(prevEncoderVals[2]);
 
-      case 'c': //kalibrer linjefølger
-        //Serial.println('c');
-        lineColor = false;
-        if(!isCalibrated) {
-          calibrateLineSensors();
-          isCalibrated = true;
-        }
-        break;
+      prevStallMillis = millis();
+   }
 
-      case 'C': //kalibrer linjefølger
-        //Serial.println('C');
-        lineColor = true;
-        if(!isCalibrated) {
-          calibrateLineSensors();
-          isCalibrated = true;
-        }
-        break;
+   if (Serial1.available()) {
+      char received = (char)Serial1.read();
+      // Serial.print("recieved command: ");
+      // Serial.println(received); //Print data to Serial Monitor
+
+      switch (received) {
+      case 'k': // Kjør!
+         // Serial.println('k');
+         while (Serial1.available() < 2) {
+            delayMicroseconds(microSecPerByte);
+         }
+
+         for (int i = 0; i < 2; i++) {
+            if (Serial1.available()) {
+               data[i] = Serial1.read();
+               // Serial.print(data[i]);
+            }
+         }
+
+         // Serial.print(data[0]);
+         // Serial.print("  ");
+         // Serial.println(data[1]);
+         accelerate(data[0], data[1]);
+         break;
+
+      case 'c': // kalibrer linjefølger
+         // Serial.println('c');
+         lineColor = false;
+         if (!isCalibrated) {
+            calibrateLineSensors();
+            isCalibrated = true;
+         }
+         break;
+
+      case 'C': // kalibrer linjefølger
+         // Serial.println('C');
+         lineColor = true;
+         if (!isCalibrated) {
+            calibrateLineSensors();
+            isCalibrated = true;
+         }
+         break;
 
       case 'w': // connected to wifi
-        digitalWrite(LED_PIN, HIGH);
-        break;
+         digitalWrite(LED_PIN, HIGH);
+         break;
 
       default:
-        break;
-    }
-
-    if(Serial1.available() >= 20){
-      while(Serial1.available() > 10) {
-        Serial1.read();
+         break;
       }
-    }
-  }
 
-  if((millis()-prevReadMillis)>(1000/readPerSec)) {
-    //Dette tar 13-14ms
+      if (Serial1.available() >= 20) {
+         while (Serial1.available() > 10) {
+            Serial1.read();
+         }
+      }
+   }
 
-    prevReadMillis = millis();
+   if ((millis() - prevReadMillis) > (1000 / readPerSec)) {
+      // Dette tar 13-14ms
 
-    proxSensors.read();
-    int8_t leftValue = proxSensors.countsFrontWithLeftLeds();
-    int8_t rightValue = proxSensors.countsFrontWithRightLeds();
-    Serial1.write(leftValue+rightValue);
+      prevReadMillis = millis();
 
-    //pos = (int8_t)((lineSensors.readLine(lineSensorValues)/20)-100); //svart linje
-    pos = (int8_t)((lineSensors.readLine(lineSensorValues,QTR_EMITTERS_ON,lineColor)/20)-100);
-    //Serial.println(pos);
-    Serial1.write(pos);
+      proxSensors.read();
+      int8_t leftValue = proxSensors.countsFrontWithLeftLeds();
+      int8_t rightValue = proxSensors.countsFrontWithRightLeds();
+      Serial1.write(leftValue + rightValue);
 
-    //Serial.println(prevReadMillis);
-  }
+      // pos = (int8_t)((lineSensors.readLine(lineSensorValues)/20)-100); //svart linje
+      pos = (int8_t)((lineSensors.readLine(lineSensorValues, QTR_EMITTERS_ON, lineColor) / 20) - 100);
+      // Serial.println(pos);
+      Serial1.write(pos);
+
+      // Serial.println(prevReadMillis);
+   }
 }
